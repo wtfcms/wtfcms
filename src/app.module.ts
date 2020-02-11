744263909ea73b5ab97fc705e129f1f1533c8659
wujingquan <i@wujingquan.com>
2020-02-11 22:43:56 +0800

    config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,14 @@ import { AdminModule } from './admin/admin.module';
 import { ApiModule } from './api/api.module';
 import { ServicesModule } from './services/services.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MikroOrmModule.forRoot({
       type: 'mongo',
-      clientUrl:
-        '',
+      clientUrl: (new ConfigService()).get<string>('DB_URL'),
       dbName: 'doracms2',
       entities: entities.entityAll(),
       entitiesDirsTs: ['src/entities'],
